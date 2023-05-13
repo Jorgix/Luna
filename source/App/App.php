32f66293bf66ccc6955ba452e18ad8b26abc55c3
Jorge Luiz <smith.exe@hotmail.com>
2023-05-13 14:35:46 -0400

    Adição do controlador do Dashboard

--- source/App/App.php
+++ source/App/App.php
@@ -2,9 +2,11 @@
 
 namespace Source\App;
 
-use Source\Core\Controller;
 use Source\Models\Auth;
+use Source\Core\Controller;
 use Source\Support\Message;
+use Source\Models\Report\Access;
+use Source\Models\Report\Online;
 
 /**
  * Class App
@@ -12,17 +14,23 @@ use Source\Support\Message;
  */
 class App extends Controller
 {
+    /** @var */
+    private $user;
+
     /**
      * App constructor.
      */
     public function __construct()
     {
-        parent::__construct(__DIR__ . "/../../themes/" . CONF_VIEW_APP);
+        parent::__construct(__DIR__ . "/../../themes/" . CONF_VIEW_APP . "/");
 
-        if (!Auth::user()) {
+        if (!$this->user = Auth::user()) {
             $this->message->warning("Efetue login para acessar o APP.")->flash();
             redirect("/entrar");
         }
+
+        (new Access())->report();
+        (new Online())->report();
     }
 
     /**
@@ -30,9 +38,7 @@ class App extends Controller
      */
     public function home()
     {
-        echo flash();
-        var_dump(Auth::user());
-        echo "<a title='Sair' href='" . url("/app/sair") . "'>Sair</a>";
+        echo $this->view->render("home", []);
     }
 
     /**
